ac996636f77a63dbb02526362173b4c295e33790
sebasdelalv340 <148891336+sebasdelalv340@users.noreply.github.com>
2025-02-11 21:47:10 +0100

    init commit

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+134ad085a7d5ea413a34674654cdaa3cc6ba93dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7fd14432d46b1274ade59da9f4217d078d07242")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
